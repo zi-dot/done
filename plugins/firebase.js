f688a13e519c8a2d8cc1cb4c0a01c8ba91c3563e
Ryoji Kono <k.ryoji.74@gmail,.com>
2020-01-27 21:55:39 +0900

    firebase setup

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
@@ -9,8 +9,7 @@ if (!firebase.apps.length) {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   });
-  firebase.analytics();
 }
 export default firebase;
