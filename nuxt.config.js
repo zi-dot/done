0ba5f174daffbf7199aa28b6ed8999459e3db3f4
Ryoji Kono <k.ryoji.74@gmail,.com>
2020-03-12 13:16:55 +0900

    set firebase plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,10 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: '~/plugins/injectEnv.js' }],
+  plugins: [
+    { src: '~/plugins/injectEnv.js' },
+    { src: '~/plugins/firebase.js', mode: 'client' }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
