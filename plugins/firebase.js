b10685be413e490a9ccb59c60bb8a541810b08a5
Ryoji Kono <k.ryoji.74@gmail,.com>
2020-03-12 13:34:59 +0900

    testing

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,24 @@
 import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  });
-}
-export default firebase;
+export default (context, inject) => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: context.app.$env.FIREBASE_API_KEY,
+      authDomain: context.app.$env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: context.app.$env.FIREBASE_DATABASE_URL,
+      projectId: context.app.$env.FIREBASE_PROJECT_ID,
+      storageBucket: context.app.$env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: context.app.$env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: context.app.$env.FIREBASE_APP_ID,
+      measurementId: context.app.$env.FIREBASE_MEASUREMENT_ID
+    });
+  }
+
+  inject('firebase', firebase);
+  inject('firestore', firebase.firestore());
+  inject('auth', firebase.auth());
+
+  return firebase;
+};
