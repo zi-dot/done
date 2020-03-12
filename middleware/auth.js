7cf6c0cafb8ba0cd2a672876a9e1d0e004600fbe
Ryoji Kono <k.ryoji.74@gmail,.com>
2020-03-12 13:49:44 +0900

    comment out middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
-import firebase from '@/plugins/firebase';
-
-export default function({ redirect, route, store }) {
-  const user = firebase.auth().currentUser;
-  console.log(user);
-  if (!user && route.path !== '/') {
-    redirect('/');
-  }
-}
+// import firebase from '@/plugins/firebase';
+//
+// export default function({ redirect, route, store }) {
+//   const user = firebase.auth().currentUser;
+//   console.log(user);
+//   if (!user && route.path !== '/') {
+//     redirect('/');
+//   }
+// }
