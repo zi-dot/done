cd89829175fabf5a6f2d176cd35246883e89463e
Ryoji Kono <k.ryoji.74@gmail,.com>
2020-03-12 16:43:46 +0900

    time out to firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/awesomeInjectEnv.js', '~/plugins/firebase.js'],
+  plugins: ['~/plugins/InjectEnv.js', '~/plugins/firebase.js'],
   /*
    ** Nuxt.js dev-modules
    */
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,18 +2,20 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-console.log('firebase setup');
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  });
-}
-console.log('finish setup');
-export default firebase;
+setTimeout(() => {
+  console.log('firebase setup');
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    });
+  }
+  console.log('finish setup');
+  export default firebase;
+}, 1000);
